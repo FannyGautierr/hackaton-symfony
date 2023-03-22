bde1ac7833b593a633b4fa5ce4446e991dbaf64e
MortyOW <adam.simon@edu.devinci.fr>
2023-03-22 14:33:06 +0100

    :bug: fixed db bug

--- skeleton/migrations/Version20230322133128.php
+++ skeleton/migrations/Version20230322133128.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230320160624 extends AbstractMigration
+final class Version20230322133128 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,14 @@ final class Version20230320160624 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE domain (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, logo VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE ski_lift (id INT AUTO_INCREMENT NOT NULL, station_id INT NOT NULL, name VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, open TIME NOT NULL, close TIME NOT NULL, information VARCHAR(255) NOT NULL, INDEX IDX_9CD969D21BDB235 (station_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE ski_track (id INT AUTO_INCREMENT NOT NULL, station_id INT NOT NULL, name VARCHAR(255) NOT NULL, difficulty VARCHAR(255) NOT NULL, open TIME NOT NULL, close TIME NOT NULL, exception TINYINT(1) NOT NULL, information LONGTEXT NOT NULL, INDEX IDX_5191EBFC21BDB235 (station_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE station (id INT AUTO_INCREMENT NOT NULL, domaine_id INT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, logo VARCHAR(255) DEFAULT NULL, INDEX IDX_9F39F8B14272FC9F (domaine_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE ski_lift ADD CONSTRAINT FK_9CD969D21BDB235 FOREIGN KEY (station_id) REFERENCES station (id)');
         $this->addSql('ALTER TABLE ski_track ADD CONSTRAINT FK_5191EBFC21BDB235 FOREIGN KEY (station_id) REFERENCES station (id)');
-        $this->addSql('ALTER TABLE station ADD CONSTRAINT FK_9F39F8B14272FC9F FOREIGN KEY (domaine_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE station ADD CONSTRAINT FK_9F39F8B14272FC9F FOREIGN KEY (domaine_id) REFERENCES domain (id)');
     }
 
     public function down(Schema $schema): void
@@ -35,6 +36,7 @@ final class Version20230320160624 extends AbstractMigration
         $this->addSql('ALTER TABLE ski_lift DROP FOREIGN KEY FK_9CD969D21BDB235');
         $this->addSql('ALTER TABLE ski_track DROP FOREIGN KEY FK_5191EBFC21BDB235');
         $this->addSql('ALTER TABLE station DROP FOREIGN KEY FK_9F39F8B14272FC9F');
+        $this->addSql('DROP TABLE domain');
         $this->addSql('DROP TABLE ski_lift');
         $this->addSql('DROP TABLE ski_track');
         $this->addSql('DROP TABLE station');
