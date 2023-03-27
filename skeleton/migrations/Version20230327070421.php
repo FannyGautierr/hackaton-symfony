f8d7e4bfd7ff4302faae146ae50ef9622841a572
MortyOW <adam.simon@edu.devinci.fr>
2023-03-27 09:11:40 +0200

    :bug: fix last minute bug

--- skeleton/migrations/Version20230327070421.php
+++ skeleton/migrations/Version20230327070421.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230326213531 extends AbstractMigration
+final class Version20230327070421 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,6 +22,10 @@ final class Version20230326213531 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE comm_post (id INT AUTO_INCREMENT NOT NULL, user_id_id INT NOT NULL, date DATETIME NOT NULL, category VARCHAR(255) NOT NULL, content VARCHAR(501) DEFAULT NULL, INDEX IDX_15AAC1F29D86650F (user_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE comm_post ADD CONSTRAINT FK_15AAC1F29D86650F FOREIGN KEY (user_id_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE ski_lift CHANGE information information VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE ski_track ADD ski_lift_id INT DEFAULT NULL, CHANGE information information LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE ski_track ADD CONSTRAINT FK_5191EBFC51447D0B FOREIGN KEY (ski_lift_id) REFERENCES ski_lift (id)');
+        $this->addSql('CREATE INDEX IDX_5191EBFC51447D0B ON ski_track (ski_lift_id)');
     }
 
     public function down(Schema $schema): void
@@ -29,5 +33,9 @@ final class Version20230326213531 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE comm_post DROP FOREIGN KEY FK_15AAC1F29D86650F');
         $this->addSql('DROP TABLE comm_post');
+        $this->addSql('ALTER TABLE ski_lift CHANGE information information VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE ski_track DROP FOREIGN KEY FK_5191EBFC51447D0B');
+        $this->addSql('DROP INDEX IDX_5191EBFC51447D0B ON ski_track');
+        $this->addSql('ALTER TABLE ski_track DROP ski_lift_id, CHANGE information information LONGTEXT NOT NULL');
     }
 }
